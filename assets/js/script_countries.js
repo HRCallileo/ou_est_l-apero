9a280f184ed65e060f00c69a7207c796a6d98042
HRCallileo <leohrc76@gmail.com>
2024-04-15 17:56:32 +0200

    mods in full project

--- assets/js/script_countries.js
+++ assets/js/script_countries.js
@@ -1,64 +1,54 @@
-// Fonction pour afficher les pays avec leurs informations, triés par heure la plus proche de 21h
-async function displayCountries(countries) {
-    // Tri des pays en fonction de l'heure la plus proche de 21h
-    countries.sort((a, b) => {
-        const aTime = getLocalTime(a.timezone);
-        const bTime = getLocalTime(b.timezone);
-        const aDiff = Math.abs(21 - parseInt(aTime.split(':')[0]));
-        const bDiff = Math.abs(21 - parseInt(bTime.split(':')[0]));
-        return aDiff - bDiff;
-    });
+// Get the element where countries will be displayed
+const countriesList = document.getElementById('countriesList');
 
-    const countriesList = document.getElementById('countriesList');
-    countries.forEach(country => {
-        const currentTime = new Date();
-        currentTime.setTime(currentTime.getTime() + currentTime.getTimezoneOffset() * 60 * 1000 + getTimeZoneOffset(country.timezone) * 60 * 60 * 1000);
-        const hours = currentTime.getHours().toString().padStart(2, '0');
-        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-        const timeString = `${hours}:${minutes}`;
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
-
-        // Changement de fond pour les pays entre 19h et 21h
-        const countryHour = currentTime.getHours();
-        if (countryHour >= 19 && countryHour < 21) {
-            listItem.classList.add('bg-warning');
-        }
-
-        listItem.textContent = `${country.name} - ${timeString}`;
-        countriesList.appendChild(listItem);
-    });
-}
-
-// Fonction pour charger les données des pays depuis le fichier JSON
+// Async function to load country data from a JSON file
 async function loadCountriesData() {
     try {
         const response = await fetch('../assets/js/countries.json');
         const countries = await response.json();
         return countries;
     } catch (error) {
-        console.error('Une erreur s\'est produite lors du chargement des données des pays :', error);
+        console.error('Une erreur s\'est produite lors du chargement des données des pays:', error);
     }
 }
 
-// Fonction pour obtenir l'heure locale du pays
-function getLocalTime(timezone) {
-    const currentTime = new Date();
-    currentTime.setTime(currentTime.getTime() + currentTime.getTimezoneOffset() * 60 * 1000 + getTimeZoneOffset(timezone) * 60 * 60 * 1000);
-    const hours = currentTime.getHours().toString().padStart(2, '0');
-    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
+// Function to display countries with their information
+function displayCountries(countries) {
+    countries.forEach(country => {
+        const flagUrl = `../assets/flags/${country.code}.png`;
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col-md-4');
+
+        // Get current date and time in the country's time zone, formatted in French
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            timeZone: country.timezone,
+            timeZoneName: 'short',
+            locale: 'fr-FR' // Set locale to French
+        };
+        const countryDateTime = new Date().toLocaleString('fr-FR', options);
+
+        cardDiv.innerHTML = `
+            <div class="card mb-3 bg-secondary border-2 border-white text-white">
+                <img src="${flagUrl}" class="card-img-top" alt="${country.name} Flag">
+                <div class="card-body">
+                    <h5 class="card-title text-center text-decoration-underline">${country.name}</h5><br>
+                    <p class="card-text text-center text-decoration-underline">Date et heure actuelles :</p>
+                    <p class="card-text text-center">${countryDateTime}</p>
+                </div>
+            </div>
+        `;
+
+        countriesList.appendChild(cardDiv);
+    });
 }
 
-// Fonction pour obtenir le décalage horaire du fuseau horaire par rapport à l'heure UTC
-function getTimeZoneOffset(timezone) {
-    const timezoneDate = new Date().toLocaleString("en-US", { timeZone: timezone });
-    const utcDate = new Date(timezoneDate);
-    const localDate = new Date();
-    const offset = (utcDate.getTime() - localDate.getTime()) / (1000 * 60 * 60);
-    return offset;
-}
-
+// Load country data and display it
 loadCountriesData()
     .then(countries => displayCountries(countries))
-    .catch(error => console.error('Une erreur s\'est produite :', error));
+    .catch(error => console.error('Une erreur s\'est produite:', error));
